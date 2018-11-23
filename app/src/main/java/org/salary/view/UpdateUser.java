5e525aea87bf236d0345982c2f302dfe30736900
Hungnm <hungnm3112@gmail.com>
2018-11-23 22:35:47 +0700

    add activity add/update user

---
+++ app/src/main/java/org/salary/view/UpdateUser.java
@@ -0,0 +1,15 @@
+package org.salary.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.salary.R;
+
+public class UpdateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user);
+    }
+}
